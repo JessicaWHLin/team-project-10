42f622ee3db1397fba90405c06b353d36be6053b
李庭均 <san222631@gmail.com>
2024-07-17 11:58:20 +0200

    1st combine - conflict between realtime.css and common.css, waited for solving

---
+++ js/realtime.js
@@ -0,0 +1,188 @@
+const x = document.getElementById("demo");
+
+const stationToCityMap = new Map([
+  ["466850", "新北市"],
+  ["466881", "新北市"],
+  ["466900", "新北市"],
+  ["466910", "臺北市"],
+  ["466920", "臺北市"],
+  ["466930", "臺北市"],
+  ["466940", "基隆市"],
+  ["466950", "基隆市"],
+  ["466990", "花蓮縣"],
+  ["467050", "桃園市"],
+  ["467080", "宜蘭縣"],
+  ["467110", "金門縣"],
+  ["467270", "彰化縣"],
+  ["467280", "苗栗縣"],
+  ["467300", "澎湖縣"],
+  ["467350", "澎湖縣"],
+  ["467410", "臺南市"],
+  ["467420", "臺南市"],
+  ["467441", "高雄市"],
+  ["467480", "嘉義市"],
+  ["467490", "臺中市"],
+  ["467530", "嘉義縣"],
+  ["467540", "臺東縣"],
+  ["467550", "南投縣"],
+  ["467571", "新竹縣"],
+  ["467590", "屏東縣"],
+  ["467610", "臺東縣"],
+  ["467620", "臺東縣"],
+  ["467650", "南投縣"],
+  ["467660", "臺東縣"],
+  ["467790", "屏東縣"],
+  ["467990", "連江縣"]
+]);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, handleLocationError);
+  } else { 
+    x.innerHTML = "即時天氣";
+  }
+}
+
+function handleLocationError(error) {
+  console.warn('Error getting location:', error.message);
+  const defaultLat = 25.09108;
+  const defaultLng = 121.5598;
+  showPosition({
+    coords: {
+      latitude: defaultLat,
+      longitude: defaultLng
+    }
+  });
+}
+
+function showPosition(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.nlsc.gov.tw/other/TownVillagePointQuery/${lng}/${lat}/4326`;
+
+  fetch(url)
+  .then(response => response.text())
+  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  .then(data => {
+      const ctyName = data.querySelector('ctyName');
+      if (!ctyName) {
+          console.error('City name not found in the response');
+          return;
+      }
+      fetchWeather(ctyName.textContent);
+      let stationIds = [];
+      for (let [key, value] of stationToCityMap.entries()) {
+        if (value === ctyName.textContent) {
+          stationIds.push(key);
+        }
+      }
+      if (stationIds.length > 0) {
+        fetchUV(stationIds);
+      } else {
+        console.log("No station IDs found for", locationName);
+      }
+  })
+  .catch(error => console.error('Error:', error));
+}
+
+function fetchWeather(locationName) {
+  const url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001";
+
+  const params = new URLSearchParams({
+      'Authorization': CWB_API_KEY,
+      'locationName': locationName
+  });
+
+  fetch(`${url}?${params.toString()}`)
+  .then(response => response.json())
+  .then(data => {
+    showBigBoxContent(data)
+    showRainfallRate(data)
+})
+  .catch(error => console.error('Error fetching weather:', error));
+}
+
+function fetchUV(stationIds) {
+  const url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0005-001";
+  const params = new URLSearchParams({
+      'Authorization': CWB_API_KEY
+  });
+
+  stationIds.forEach(id => params.append('StationID', id));
+
+  fetch(`${url}?${params.toString()}`)
+  .then(response => response.json())
+  .then(data => {
+    showUVrays(data)
+  })
+  .catch(error => console.error('Error fetching UV:', error));
+}
+
+function showBigBoxContent(data){
+  const weatherElements = data.records.location[0].weatherElement;
+  
+    const wxElement = weatherElements.find(el => el.elementName === 'Wx');
+    const wxDescription = wxElement.time[0].parameter.parameterName;
+
+    const minTElement = weatherElements.find(el => el.elementName === 'MinT');
+    const minTValue = minTElement.time[0].parameter.parameterName;
+
+    const maxTElement = weatherElements.find(el => el.elementName === 'MaxT');
+    const maxTValue = maxTElement.time[0].parameter.parameterName;
+
+    const averageTemp = (parseFloat(minTValue) + parseFloat(maxTValue)) / 2;
+
+    document.getElementById('temp-value').textContent = `${averageTemp}`;
+    document.getElementById('temperature-description').textContent = wxDescription;
+    document.querySelectorAll('.temp-range-value')[0].textContent = minTValue;
+    document.querySelectorAll('.temp-range-value')[1].textContent = maxTValue;
+
+    const locationName = data.records.location[0].locationName;
+    document.getElementById('city-name').textContent = locationName;
+
+    const ciElement = weatherElements.find(el => el.elementName === 'CI');
+    const ciDescription = ciElement.time[0].parameter.parameterName;
+
+    document.getElementById('city-temp-feel').textContent = ciDescription
+
+    const now = new Date();
+
+    const weekday = now.toLocaleDateString('zh-TW', { weekday: 'long' }); 
+    const day = now.getDate(); 
+
+    const dateString = `${weekday} ${day}`; 
+
+    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }); 
+    document.getElementById('date').textContent = dateString;
+    document.getElementById('time').textContent = timeString;
+}
+
+function showRainfallRate(data) {
+  const weatherElements = data.records.location[0].weatherElement;
+  const popElement = weatherElements.find(element => element.elementName === 'PoP');
+  if (popElement && popElement.time.length > 0) {
+      const firstTime = popElement.time[0];
+      const rainfallRate = `${firstTime.parameter.parameterName}%`; 
+
+      document.getElementById('rainfall-rate').textContent = rainfallRate;
+  } else {
+      console.error('No PoP data available or data structure is incomplete.');
+  }
+}
+
+function showUVrays(data) {
+  const uvRaysContainer = document.getElementById('uv-rays');
+  if (data.records.weatherElement.location.length === 0) {
+    uvRaysContainer.textContent = '無資料';
+  } else {
+    const maxUVIndex = data.records.weatherElement.location.reduce((max, item) => {
+      return item.UVIndex > max.UVIndex ? item : max;
+    }, { UVIndex: -Infinity }); 
+
+    uvRaysContainer.textContent = `${maxUVIndex.UVIndex}`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+  getLocation()
+});
