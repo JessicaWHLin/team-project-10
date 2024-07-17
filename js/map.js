a5056ca6cba6356b4843a46edb524644b40ea85e
twkclaire <twkclaire@gmail.com>
2024-07-17 19:46:27 +0200

    adding tooltip

--- js/map.js
+++ js/map.js
@@ -14,26 +14,30 @@ function highlightCity(cityName) {
     });
 }
 
-// delete this one when merge location 
-function getUserInputAndHighlight() {
-    const userInput = prompt("你在哪個城市？"); 
-    if (userInput) {
-        highlightCity(userInput);
-    }
-}
+
 
 // if user click map 
-const p = document.querySelectorAll("path");
-p.forEach(function(item) {
-    item.addEventListener("click", function() {
-        cityName = item.getAttribute("data-name");
+const tooltip = document.getElementById("tooltip");
+const paths = document.querySelectorAll("path");
+paths.forEach(path=> {
+    path.addEventListener("click", function(event) {
+        cityName = event.target.getAttribute("data-name");
+        tooltip.innerHTML=cityName;
+        tooltip.style.display="block";
         console.log(cityName);
-        highlightCity(cityName);  
+        highlightCity(cityName);
     });
+        path.addEventListener("mousemove", (event) => {
+        tooltip.style.left = event.pageX + 10 + "px";
+        tooltip.style.top = event.pageY + 10 + "px";
+      });
+  
+      path.addEventListener("mouseout", () => {
+        tooltip.style.display = "none";
+      });
 });
 
 // Trigger user input prompt on load
 getUserInputAndHighlight();
 
-  
 
